e0a2145750294a2032c742021ffd487589441cf7
Davis <justinkdavis@ou.edu>
2020-06-17 12:35:08 -0500

    changes to ecograph vignette

--- vignettes/malaria vignette.R
+++ vignettes/malaria vignette.R
@@ -7,17 +7,19 @@ load("c:\\home\\Dropbox (Univ. of Oklahoma)\\work\\neko\\for_others\\20200615_bb
 reg_eq <- formula("log(cases_epidemiar+1) ~ woreda_name +
                     s(doy, bs = 'cc', by=woreda_name, id = 1) +
                     s(numericdate, by = woreda_name, bs = 'tp', id = 2) +
-                    s(lag, by = lst_day, bs = 'tp', id = 3) + 
+                    s(lag, by = lst_day, bs = 'tp', id = 3) +
                     s(lag, by = ndwi6, bs = 'tp', id = 4) +
                     s(lag, by = totprec, bs = 'tp', id = 5)")
 reg_eq_fallback <- formula("log(cases_epidemiar+1) ~ 1 +
                     s(doy, bs = 'cc', id = 1) +
                     s(numericdate, bs = 'tp', id = 2) +
-                    s(lag, by = lst_day, bs = 'tp', id = 3) + 
+                    s(lag, by = lst_day, bs = 'tp', id = 3) +
                     s(lag, by = ndwi6, bs = 'tp', id = 4) +
                     s(lag, by = totprec, bs = 'tp', id = 5)")
+reg_eq_broken  <- formula("log(cases_epidemiar+1) ~ oops")
 fc_model_family <- gaussian()
 
+# time regressions with no error
 starttime <- proc.time()
 
 regress <- clusterapply::batch_bam(data = epi_input_tp,
@@ -35,6 +37,15 @@ epi_input_tp$preds <- clusterapply::predict.batch_bam(models=regress,
 stoptime <- proc.time() - starttime
 stoptime
 
+# plot obs vs. pred
 ggplot(epi_input_tp) + geom_point(aes(x=log(cases_epidemiar+1), y=preds)) +
   geom_abline(slope=1, intercept=0, linetype=2, size=2, color="red") +
   coord_equal()
+
+# what happens when you have an error but have a backup equation?
+regress <- clusterapply::batch_bam(data = epi_input_tp,
+                                   bamargs = list("formula" = reg_eq_broken,
+                                                  "family" = fc_model_family,
+                                                  "discrete" = TRUE),
+                                   bamargs_fallback = list("formula" = reg_eq),
+                                   over = "cluster_id")
