11e0b2f64719b380e8abb737cefeb814467c594b
michdn <28901045+michdn@users.noreply.github.com>
2021-03-04 14:53:14 -0600

    Added missing argument description

--- R/applytoeachinlist.R
+++ R/applytoeachinlist.R
@@ -3,25 +3,28 @@
 #'
 #' @export
 #'
-#' @param listobject A named list object containing elements to which
-#'   the applyfun will be applied.
+#' @param listobject A named list object containing elements to which the
+#'   applyfun will be applied.
 #' @param applyfun The character name of a function to apply.
 #' @param applyargs A named list of arguments supplied to applyfun.
+#' @param fallbackargs A named list of arguments to try in case the primary
+#'   `applyargs` fail.
 #' @param splitalongside An optional dataframe that should be split alongside
 #'   the listobject, containing the factor variable splitalongsidename. For
-#'   example, if listobject is a list of regressions, then splitalongside
-#'   may be a set of newdata, indexed so that predictions for each regression
-#'   depend only on some subset of splitalongside
-#' @param nameaftersplit The name of the argument of applyfun that
-#'   takes the settosplit dataframe after it has been split; this is
-#'   commonly 'x' or 'data'
-#' @param splitalongsidename what should the splitalongside frame be called after
-#'   it has been split? This is commonly something like "newdata"
-#' @param splitalongsidesplitter The name of the factor variable in settosplit, over
-#'   which it should be split.
+#'   example, if listobject is a list of regressions, then splitalongside may be
+#'   a set of newdata, indexed so that predictions for each regression depend
+#'   only on some subset of splitalongside
+#' @param nameaftersplit The name of the argument of applyfun that takes the
+#'   settosplit dataframe after it has been split; this is commonly 'x' or
+#'   'data'
+#' @param splitalongsidename what should the splitalongside frame be called
+#'   after it has been split? This is commonly something like "newdata"
+#' @param splitalongsidesplitter The name of the factor variable in settosplit,
+#'   over which it should be split.
 #' @return This function returns a named list of results, having applied
 #'   applyfun to settosplit for every level of the 'over' variable. So for
-#'   example, applytoeachinlist(...)[["a"]] is the result of applyfun(listobject[["a"]]).
+#'   example, applytoeachinlist(...)[["a"]] is the result of
+#'   applyfun(listobject[["a"]]).
 
 applytoeachinlist <- function(listobject=NULL,
                               applyfun=NULL,
